f26143cefcd777cc80581f2f273624fc134aac7f
microlive <microliveindia@gmail.com>
2014-08-06 19:57:11 +0530

    Artinsive Interior

---
+++ services.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en" class="ie ie9"> <!--<![endif]-->
+
+<!-- Mirrored from www.gt3themes.com/website-templates/interior/gallery.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Jul 2014 01:53:41 GMT -->
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="content-type"/>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<!-- Mobile Specific Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="img/ico/favicon.ico">
+<title>Modern Interios - HTML5 &amp; CSS3 portfolio fullscreen theme</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">    
+<link rel="stylesheet" type="text/css" href="css/plugins.css">
+<link rel="stylesheet" type="text/css" href="css/theme.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+<link rel="stylesheet" type="text/css" href="css/theme_settings.css">        
+<!--[if IE 8 ]><script>
+    var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
+    for (var i = 0; i < e.length; i++) {
+        document.createElement(e[i]);
+    }		
+</script><![endif]-->
+<script type="text/javascript" src="code.jquery.com/jquery-1.9.1.min.js"></script>
+</head>
+<body>
+	<div class="main_wrapper">
+        <!--header-->
+       <?php
+				include 'nav.php';
+				?>
+        <!--//header-->
+        
+        <!-- C O N T E N T -->
+        <div class="content_wrapper">
+            <div class="container">
+                <div class="content_block right-sidebar row">
+                    <div class="fl-container span9">
+                        <div class="row">
+                            <div class="posts-block span9">
+                                <div class="contentarea">
+                                
+                                    <div class="row-fluid">
+                                        <div class="span12">
+                                            <h1 class="headInModule">Our Services</h1>
+                                        </div>								
+                                    </div><!-- .row-fluid -->
+                                    <div class="row-fluid">
+                                        <div class="span12 module_cont module_text_area">
+                                           
+<p>Our interior designing and decoration service areas include: Domestic   Homes, Business Establishments, Educational Institutes, Beauty Parlours /   Salons, Offices, Devotional / Prayer Houses, NGO Establishments and   more....<br>
+  <br>
+In preparation for projects, our team researches the history   of a site            and seek to determine the most cost effective and   aesthetically appropriate solution. We identify the thematic styles   which most effectively work within budgetary restraints and present them   as a series of concepts.            Our discussions with the client seek to explore ideas and   identify preferences. Only when a client feels comfortable do we proceed   with the            execution for delivering the best product with genuine   enthusiasm and effective cost management. Every step of the process is   documented to be easily interpreted and reviewed for evaluating   purposes.<br>
+<br>
+The final stage of our process might include a variety of   service options. Our scope of capabilities includes space planning,   interior finish and lighting specification, high-end millwork design,   and artwork/accessories. We fully invest our resources in the field and   work within each trade to deliver the best possible product. </p>
+                                            <hr class="content_type">
+                                        </div>								
+                                    </div><!-- .row-fluid -->
+                                    
+                                  
+                                
+                                </div><!-- .contentarea -->
+                            </div>                                                                
+                            <div class="left-sidebar-block span3">
+                                <aside class="sidebar">
+                                </aside>
+                            </div><!-- .left-sidebar -->
+                        </div>
+                        <div class="clear"><!-- ClearFix --></div>
+                    </div><!-- .fl-container -->
+                    <div class="right-sidebar-block span3">
+                        <aside class="sidebar">
+                        	<div class="sidepanel widget_search">
+                                <form class="search_form" action="#" method="get" name="search_form">
+                                	<input class="field_search" type="text" title="Search the Site" value="Search the Site" name="s">
+                                </form>
+                            </div>
+                            <div class="sidepanel widget_nav_menu">
+                                <div class="bg_title"><h4 class="sidebar_header">Quick Links</h4></div>
+                                <ul class="menu">
+                                    <li class="menu-item"><a href="#">About Us</a></li>
+                                    <li class="menu-item"><a href="#">Feedback Form</a></li>
+                                    <li class="menu-item"><a href="#">Gallery</a></li>
+                                    <li class="menu-item"><a href="#">Featured Works</a></li> 
+                                    <li class="menu-item"><a href="#">Contact Us</a></li>               
+                                </ul>
+                            </div>                      
+
+                            <!-- .sidepanel -->
+                            
+                          
+
+                            <!-- .sidepanel -->
+                        </aside>
+                    </div><!-- .right-sidebar -->
+                    <div class="clear"><!-- ClearFix --></div>
+                </div>
+            </div><!-- .container -->
+       </div><!-- .content_wrapper -->    
+    
+       <?php
+	   include 'footer.php';
+	   ?>    
+    </div><!-- //main_wrapper -->
+    <script src="code.jquery.com/ui/1.10.2/jquery-ui.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/theme.js"></script>
+</body>
+
+<!-- Mirrored from www.gt3themes.com/website-templates/interior/gallery.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Jul 2014 01:53:49 GMT -->
+</html>
